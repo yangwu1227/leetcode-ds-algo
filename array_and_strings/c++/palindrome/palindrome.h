48d82d27639ad644519e7a9a7fd9f89668276a4c
YangWu1227 <yangwu2020@gmail.com>
2024-03-26 17:34:09 -0500

    style: format code

--- array_and_strings/c++/palindrome/palindrome.h
+++ array_and_strings/c++/palindrome/palindrome.h
@@ -5,13 +5,13 @@
 #include <string>
 
 /**
- * @brief Checks if the provided string is a palindrome. A palindrome is a string that reads the same 
- * backward as forward. This function iterates over the string from both ends towards the center, comparing 
+ * @brief Checks if the provided string is a palindrome. A palindrome is a string that reads the same
+ * backward as forward. This function iterates over the string from both ends towards the center, comparing
  * characters for equality.
- * 
+ *
  * @param s The string to check, passed by constant reference to avoid copying.
  * @return true if `s` is a palindrome, false if not.
  */
-bool checkIfPalindrome(const std::string& s);
+bool checkIfPalindrome(const std::string &s);
 
 #endif
--- array_and_strings/c++/sorted_two_sum/sorted_two_sum.h
+++ array_and_strings/c++/sorted_two_sum/sorted_two_sum.h
@@ -6,13 +6,13 @@
 
 /**
  * @brief Checks if there is a pair of numbers within the sorted (ascending) vector that sums up to the target value.
- * This function assumes the input vector is sorted in ascending order and iterates through the vector to find if 
+ * This function assumes the input vector is sorted in ascending order and iterates through the vector to find if
  * such a pair exists.
- * 
+ *
  * @param nums Input vector of sorted (ascending) integers.
  * @param target Target integer.
  * @return true if there is a pair of numbers that sum to target, false if there is none.
  */
-bool checkForTarget(std::vector<int>& nums, int target);
+bool checkForTarget(std::vector<int> &nums, int target);
 
 #endif
