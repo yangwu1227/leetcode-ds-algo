97c70d481329af318a525ff5666f99e70323554c
YangWu1227 <yangwu2020@gmail.com>
2024-05-19 01:52:26 -0500

    refactor: lambda args type and const keyword

--- hashing/counting/c++/sum_unique/sum_unique.cpp
+++ hashing/counting/c++/sum_unique/sum_unique.cpp
@@ -22,7 +22,7 @@ int sumUnique(const std::vector<int> &nums)
     // Just for practice, just accumulate but a simple loop with structured bindings should do
     int uniqueSum = std::accumulate(
         nums.begin(), nums.end(), 0,
-        [&counts](auto currentCumSum, auto nextNum)
+        [&counts](int currentCumSum, const auto nextNum)
         {
             // If the next element count is 1, add it, else skip by returning current cumulative sum
             return (counts[nextNum] == 1 ? currentCumSum + nextNum : currentCumSum);
