0163f73cac41eaa08b1dce3de2766cd26e5e0ee6
YangWu1227 <yangwu2020@gmail.com>
2024-07-04 17:30:09 -0500

    feat: add node translation unit

---
+++ linked_lists/fundamentals/c++/singly/node.hpp
@@ -0,0 +1,39 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+#include <memory>
+#include <iostream>
+
+/**
+ * @class ListNode
+ * @brief A class representing a node in a singly linked list.
+ */
+class ListNode
+{
+public:
+    using Ptr = std::shared_ptr<ListNode>;
+
+    /**
+     * @brief Constructs a new ListNode object.
+     *
+     * @param data An integer representing the data associated with the node instance
+     */
+    explicit ListNode(int data = 0);
+
+    /**
+     * @brief Overloads the << operator to print the node data.
+     *
+     * @param os The output stream
+     * @param node The node to be printed
+     * @return std::ostream& The output stream with the node data
+     */
+    friend std::ostream &operator<<(std::ostream &os, const ListNode &node);
+
+private:
+    int data; // The data stored in the node
+    Ptr next; // Pointer to the next node
+
+    // Provide access to SinglyLinkedList class
+    friend class SinglyLinkedList;
+};
+
+#endif
